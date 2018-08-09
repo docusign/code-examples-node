7f22743779051737da22afc49b4da70bfeabe9e0
larry-kluger <larry.kluger@docusign.com>
2018-08-09 21:44:48 +0300

    Added APP_URL comment

--- ds_configuration.js
+++ ds_configuration.js
@@ -8,7 +8,7 @@ exports.config = {
   , dsClientSecret: process.env.DS_CLIENT_SECRET || '{CLIENT_SECRET}' // The app's DocuSign integration key's secret
   , signerEmail: process.env.DS_SIGNER_EMAIL || '{USER_EMAIL}'
   , signerName: process.env.DS_SIGNER_NAME || '{USER_NAME}'
-  , appUrl: process.env.DS_APP_URL || '{APP_URL}'
+  , appUrl: process.env.DS_APP_URL || '{APP_URL}' // The url of the application. Eg http://localhost:5000
   , production: false
   , debug: true // Send debugging statements to console
   , sessionSecret: '12345' // Secret for encrypting session cookie content
