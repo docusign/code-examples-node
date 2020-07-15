0ac65cd3fa1fec9dfff9a0432e4d2971697bcaa1
Aaron Wilde <Aaron.Wilde@docusign.com>
2020-07-14 21:22:18 -0700

    refactor into single config setup

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 exports.docOptions = require('./documentOptions.json');
 exports.docNames = require('./documentNames.json');
 const settings = require('./appsettings.json');
-const jwt = require('./jwtsettings.json');
 exports.github = require('./github.json');
 
 const dsOauthServer = settings.production
@@ -14,9 +13,9 @@ settings.signerEmail = process.env.DS_SIGNER_EMAIL || settings.signerEmail;
 settings.signerName = process.env.DS_SIGNER_NAME || settings.signerName;
 settings.dsClientId = process.env.DS_CLIENT_ID || settings.dsClientId;
 settings.appUrl = process.env.DS_APP_URL || settings.appUrl;
-settings.dsJWTClientId = process.env.DS_JWT_CLIENT_ID || jwt.dsJWTClientId;
-settings.privateKeyLocation = process.env.DS_PRIVATE_KEY_PATH  || jwt.privateKeyLocation;
-settings.impersonatedUserGuid =  process.env.DS_IMPERSONATED_USER_GUID || jwt.impersonatedUserGuid;
+settings.dsJWTClientId = process.env.DS_JWT_CLIENT_ID || settings.dsJWTClientId;
+settings.privateKeyLocation = process.env.DS_PRIVATE_KEY_PATH  || settings.privateKeyLocation;
+settings.impersonatedUserGuid =  process.env.DS_IMPERSONATED_USER_GUID || settings.impersonatedUserGuid;
 
 exports.config = {
   dsOauthServer,
