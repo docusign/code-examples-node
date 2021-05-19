a6adc741d9f4ed3fd237b0330662c63b89d4d35c
RomanBachaloSigmaSoftware <roman.bachalo@sigma.software>
2021-05-19 20:09:59 +0300

    Fixed some code style issues

--- eg001EmbeddedSigning.js
+++ eg001EmbeddedSigning.js
@@ -4,23 +4,20 @@
  * @author DocuSign
  */
 
-const path = require('path')
-    , embeddedSigning = require('./lib/eSignature/examples/embeddedSigning')
-    , validator = require('validator')
-    , dsConfig = require('./config/index.js').config
-    ;
+const path = require('path');
+const embeddedSigning = require('./lib/eSignature/examples/embeddedSigning');
+const validator = require('validator');
+const dsConfig = require('./config/index.js').config;
 
-const eg001EmbeddedSigning = exports
-    , eg = 'eg001' // This example reference.
-    , mustAuthenticate = '/ds/mustAuthenticate'
-    , minimumBufferMin = 3
-    , signerClientId = 1000 // The id of the signer within this application.
-    , demoDocsPath = path.resolve(__dirname, 'demo_documents')
-    , pdf1File = 'World_Wide_Corp_lorem.pdf'
-    , dsReturnUrl = dsConfig.appUrl + '/ds-return'
-    , dsPingUrl = dsConfig.appUrl + '/' // Url that will be pinged by the DocuSign signing via Ajax
-    ;
-
+const eg001EmbeddedSigning = exports;
+const eg = 'eg001'; // This example reference.
+const mustAuthenticate = '/ds/mustAuthenticate';
+const minimumBufferMin = 3;
+const signerClientId = 1000; // The id of the signer within this application.
+const demoDocsPath = path.resolve(__dirname, 'demo_documents');
+const pdf1File = 'World_Wide_Corp_lorem.pdf';
+const dsReturnUrl = dsConfig.appUrl + '/ds-return';
+const dsPingUrl = dsConfig.appUrl + '/'; // Url that will be pinged by the DocuSign signing via Ajax
 
 /**
  * Create the envelope, the embedded signing, and then redirect to the DocuSign signing
@@ -31,7 +28,7 @@ eg001EmbeddedSigning.createController = async (req, res) => {
     // Step 1. Check the token
     // At this point we should have a good token. But we
     // double-check here to enable a better UX to the user.
-    let tokenOK = req.dsAuth.checkToken(minimumBufferMin);
+    const tokenOK = req.dsAuth.checkToken(minimumBufferMin);
     if (! tokenOK) {
         req.flash('info', 'Sorry, you need to re-authenticate.');
         // Save the current operation so it will be resumed after authentication
@@ -40,39 +37,34 @@ eg001EmbeddedSigning.createController = async (req, res) => {
     }
 
     // Step 2. Call the worker method
-    let body = req.body
-        // Additional data validation might also be appropriate
-      , signerEmail = validator.escape(body.signerEmail)
-      , signerName = validator.escape(body.signerName)
-      , envelopeArgs = {
-            signerEmail: signerEmail,
-            signerName: signerName,
-            signerClientId: signerClientId,
-            dsReturnUrl: dsReturnUrl,
-            dsPingUrl: dsPingUrl,
-            docFile: path.resolve(demoDocsPath, pdf1File)
-        }
-      , args = {
-            accessToken: req.user.accessToken,
-            basePath: req.session.basePath,
-            accountId: req.session.accountId,
-            envelopeArgs: envelopeArgs
-        }
-      , results = null
-      ;
+    const { body } = req;
+    const envelopeArgs = {
+        signerEmail: validator.escape(body.signerEmail),
+        signerName: validator.escape(body.signerName),
+        signerClientId: signerClientId,
+        dsReturnUrl: dsReturnUrl,
+        dsPingUrl: dsPingUrl,
+        docFile: path.resolve(demoDocsPath, pdf1File)
+    };
+    const args = {
+        accessToken: req.user.accessToken,
+        basePath: req.session.basePath,
+        accountId: req.session.accountId,
+        envelopeArgs: envelopeArgs
+    };
+    let results = null;
 
     try {
-        results = await embeddedSigning.sendEnvelopeForEmbeddedSigning (args)
+        results = await embeddedSigning.sendEnvelopeForEmbeddedSigning(args);
     }
     catch (error) {
-        let errorBody = error && error.response && error.response.body
-            // we can pull the DocuSign error code and message from the response body
-          , errorCode = errorBody && errorBody.errorCode
-          , errorMessage = errorBody && errorBody.message
-          ;
+        const errorBody = error && error.response && error.response.body;
+        // we can pull the DocuSign error code and message from the response body
+        const errorCode = errorBody && errorBody.errorCode;
+        const errorMessage = errorBody && errorBody.message;
         // In production, may want to provide customized error messages and
         // remediation advice to the user.
-        res.render('pages/error', {err: error, errorCode: errorCode, errorMessage: errorMessage});
+        res.render('pages/error', {err: error, errorCode, errorMessage});
     }
     if (results) {
         // Redirect the user to the embedded signing
@@ -91,7 +83,7 @@ eg001EmbeddedSigning.getController = (req, res) => {
     // Check that the authentication token is ok with a long buffer time.
     // If needed, now is the best time to ask the user to authenticate
     // since they have not yet entered any information into the form.
-    let tokenOK = req.dsAuth.checkToken();
+    const tokenOK = req.dsAuth.checkToken();
     if (tokenOK) {
         res.render('pages/examples/eg001EmbeddedSigning', {
             eg: eg, csrfToken: req.csrfToken(),
